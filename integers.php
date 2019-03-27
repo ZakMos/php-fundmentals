c847c7b39110b1c6ac8e0100c321304308b33949
ZakMos <z.zza@hotmail.com>
2019-03-27 18:50:32 +0100

    integers

---
+++ integers.php
@@ -0,0 +1,19 @@
+<?php
+  $regInt = -1234;
+  $octNum = 01234;
+  $hexNum = 0xABC;
+  $binaryNum = 0b1000;
+
+echo('<h1>Integers</h1>');
+echo "regInt= ";
+  var_dump($regInt);
+
+  echo ",    octNum = ";
+  var_dump($octNum);
+
+  echo ",    hexNum = ";
+  var_dump($hexNum);
+
+  echo ",    binaryNum = ";
+  var_dump($binaryNum);
+ ?>
