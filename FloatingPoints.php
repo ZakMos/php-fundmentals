2acb34c1af5f294d2eaa28f28b0124a1c7041adc
ZakMos <z.zza@hotmail.com>
2019-03-27 18:50:44 +0100

    Floating Points

---
+++ FloatingPoints.php
@@ -0,0 +1,11 @@
+<?php
+  $float = 1.234;
+  $scientific = 0.1234E4;
+  $scientific = 1234E-4;
+
+  echo ('<h1>Floating Points</h1>');
+  var_dump($float);
+  var_dump($scientific);
+  var_dump($scientific);
+
+ ?>
