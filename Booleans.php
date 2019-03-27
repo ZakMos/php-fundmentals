a6b8d71172acb2da88e4f33087bb32fcf1e35cbf
ZakMos <z.zza@hotmail.com>
2019-03-27 18:59:52 +0100

    Booleans

---
+++ Booleans.php
@@ -0,0 +1,16 @@
+<?php
+  $bool = false;
+  $hasValue = "Hello Everyone";
+
+  var_dump((bool)$hasValue);
+
+ ?>
+
+
+<!--
+  $bool = true; ==== true;
+  $bool = false; ==== false;
+  $hasValue = 1234; === true;
+  $hasValue = ""; === false;
+  $hasValue= 0; === false;
+ -->
