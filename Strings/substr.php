46f3acb3a70d662b78ef06282e01c58ac8e11c41
ZakMos <z.zza@hotmail.com>
2019-03-28 23:04:09 +0100

    Sub string

---
+++ Strings/substr.php
@@ -0,0 +1,8 @@
+<?php
+  $quote =  "Only those who will risk going too far can possibly find out how far one can go.";
+
+  // string substr(string $string, int $start[, int $lenth])
+  // numbers from 0 is starting string and (-) from the end of string
+  echo substr($quote, 3, -5)
+
+ ?>
