656e031bba56758f06c70bb32b69d28de8dac4a6
ZakMos <z.zza@hotmail.com>
2019-03-28 13:37:43 +0100

    Double Quotes

---
+++ Strings/DoubleQuotes.php
@@ -0,0 +1,21 @@
+<?php
+  $currency = 'penny';
+
+  $sampleString = "A $currency saved is a $currency earned";
+  // Return A penny saved is a penny earned
+
+
+  //echo $sampleString;
+
+  // concatenation
+
+  $var = 2;
+
+  echo "{$var}nd place";
+
+  echo $var."nd place";
+
+  $misc = "St. Patrick's Day";
+  $misc = "\n";
+  echo $misc;
+ ?>
