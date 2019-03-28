01ecfdad10123ba357ac8a5c5abaad9b49b25e32
ZakMos <z.zza@hotmail.com>
2019-03-28 13:49:39 +0100

    Changing Case

---
+++ Strings/ChangingCase.php
@@ -0,0 +1,13 @@
+<?php
+  $quote = "To be or not to be, that is the question.";
+
+  // To Upper Case
+  $quote = mb_strtoupper($quote);
+
+  // To Lower Case
+  $quote = mb_strtolower($quote);
+
+  echo $quote;
+
+
+ ?>
