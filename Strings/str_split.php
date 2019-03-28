a6ce1367b643f1028cbab371142d296ffe9a90e2
ZakMos <z.zza@hotmail.com>
2019-03-28 23:08:45 +0100

    String Split

---
+++ Strings/str_split.php
@@ -0,0 +1,9 @@
+<?php
+  $quote = "It is a melancholy truth that even great men have their poor relations.";
+
+  // string str_split(string $string[, int $split_length = 1])
+  $varArray = str_split($quote, 8);
+
+  print_r($varArray);
+
+ ?>
