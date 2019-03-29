52cf051c811441d55c9566d560873a075a4551ee
ZakMos <z.zza@hotmail.com>
2019-03-29 18:12:10 +0100

    For each Loop array

---
+++ Arrays/ForeachLoop.php
@@ -0,0 +1,14 @@
+<?php
+
+$authors = array(
+        "quarky" => "Charls Dickens",
+        "brilian" => "Jane Austin",
+        "Poetic" => "William Shakspeare"
+);
+
+foreach($authors as $key => $val) {
+  echo $val."(".$key.")\n";
+}
+
+
+ ?>
