76601126d84051f1e5346ea520eaee6076ff1a65
ZakMos <z.zza@hotmail.com>
2019-03-29 15:49:08 +0100

    Associative Arrayes

---
+++ Arrays/AssociativeArrayes.php
@@ -0,0 +1,10 @@
+<?php
+  $authors = array(
+          "quarky" => "Charls Dickens",
+          "brilian" => "Jane Austin",
+          "Poetic" => "William Shakspeare",
+          10 => "number",
+          "Any Value"
+  );
+  print_r($authors);
+ ?>
