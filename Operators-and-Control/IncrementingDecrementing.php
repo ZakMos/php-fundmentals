ce5df48ac6742721a3a0af77dde61ad5fdc72644
ZakMos <z.zza@hotmail.com>
2019-04-02 10:26:20 +0200

    Incrementing and Decrementing

---
+++ Operators-and-Control/IncrementingDecrementing.php
@@ -0,0 +1,16 @@
+<?php
+
+$alterByOne = 2;
+
+$alterByOne++;
+
+// when the ++ in the left side it will add the value before it does anything else
+// with that variable
+++$alterByOne;
+
+
+$alterByOne--;
+
+echo $alterByOne;
+
+ ?>
