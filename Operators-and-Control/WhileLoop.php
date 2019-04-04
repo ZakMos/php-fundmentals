82c481b332aa82136f41793f5b7059d58e932b6e
ZakMos <z.zza@hotmail.com>
2019-04-04 20:40:01 +0200

    While Loop

---
+++ Operators-and-Control/WhileLoop.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+  while(expr) {
+  // block of code
+}
+*/
+
+$readingIsFun = true;
+
+while($readingIsFun) {
+  echo "Reading is fun!".PHP_EOL;
+  $readingIsFun = false;
+}
+
+
+$readingIsFun1 = true;
+$i = 0;
+while($i < 5) {
+  echo "Reading is fun(1)!".PHP_EOL;
+  $i++;
+}
+
+ ?>
