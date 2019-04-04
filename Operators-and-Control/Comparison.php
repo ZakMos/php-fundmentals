323d7093d93b30c15df6c76ea1876b350f2a81a4
ZakMos <z.zza@hotmail.com>
2019-04-04 16:20:52 +0200

    Comparison Operator

---
+++ Operators-and-Control/Comparison.php
@@ -0,0 +1,27 @@
+<?php
+
+var_dump(8 == 8); // output bool(true)
+
+var_dump(8 === 8); // output bool(true)
+
+var_dump(8 === "8"); // output bool(false)
+
+var_dump(7 <> 6); // output bool(true)
+
+var_dump(7 != 6); // output bool(true)
+
+var_dump(7 != "7"); // output bool(false)
+
+var_dump(7 !=- "7"); // output bool(true)
+
+var_dump(7 > 7); // output bool(false)
+
+var_dump(7 >= 7); // output bool(true)
+
+var_dump(7 <  6); // output bool(false)
+
+var_dump(7 <=  6); // output bool(false)
+
+
+
+ ?>
